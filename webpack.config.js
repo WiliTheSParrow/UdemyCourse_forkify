6c7f2809124ae65cb986d2fa87f881f9eb290908
Júlia W <w.julia91@gmail.com>
2020-07-23 10:41:12 +0200

    simple webvack set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
-
-
+    entry: './src/js/index.js',
+    output: {
+        // To include path we need a node.js package, we need absolute path:
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
 
 };
