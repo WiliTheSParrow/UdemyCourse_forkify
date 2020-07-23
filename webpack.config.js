02fedc517dc6d88c0c9d178e8453fa2aa02743b6
Júlia W <w.julia91@gmail.com>
2020-07-23 15:08:27 +0200

    babel loader added

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,21 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
             // copies the index.html from src folder to dist
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [{
+            // It'll look for all the files, which has the ending '.js'.
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
 
 };
