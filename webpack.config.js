207ae16ce0f3cce8d7845a84746a9e7f95ae30b1
Júlia W <w.julia91@gmail.com>
2020-07-23 13:57:25 +0200

    webpack plugin applied to copy html index file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
         // To include path we need a node.js package, we need absolute path:
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
     devServer: {
         contentBase: './dist'
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            // copies the index.html from src folder to dist
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 
 };
