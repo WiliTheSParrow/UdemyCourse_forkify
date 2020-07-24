f78838c7813fbb8c640ed1110edb63625a7fe0fd
Júlia W <w.julia91@gmail.com>
2020-07-24 17:13:37 +0200

    title shortened if more thank 17 char

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -18,12 +18,17 @@ export const clearResults = () => {
 // accumulator: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
 // accumulator: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato', 'and']
 // accumulator: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato', 'and', 'spinach']
-
 const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
     if (title.length > limit) {
         title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
 
         }, 0);
+        //return the result
+        return `${newTitle.join(' ')}...`;
     };
     return title;
 };
@@ -35,7 +40,7 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
