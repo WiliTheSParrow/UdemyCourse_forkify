c494bb69536bcd314998a47f202564b33e790eba
Júlia W <w.julia91@gmail.com>
2020-07-24 13:01:10 +0200

    added comment of the global state of the app

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
 import Search from './models/Search';
 
+/* Global state of the app
+- Search object
+- Current recipe object
+- SHopping list object
+- Liked recipes */
+ 
 const search = new Search('pizza');
 console.log(search);
 search.getResults();
