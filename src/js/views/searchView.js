756e436728fbecd5b5945fc1bf98066baa56b5b0
Júlia W <w.julia91@gmail.com>
2020-07-24 17:03:06 +0200

    comm added how limitRecipeTitle works

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,22 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+// 'Pasta with tomato and spinach'
+// accumulator: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+// accumulator: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+// accumulator: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+// accumulator: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato', 'and']
+// accumulator: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato', 'and', 'spinach']
+
+const limitRecipeTitle = (title, limit = 17) => {
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+
+        }, 0);
+    };
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `<li>
     <a class="results__link" href="#${recipe.recipe_id}">
