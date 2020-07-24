0080ccfe58e1b0929d46c70006c90d333165da62
Júlia W <w.julia91@gmail.com>
2020-07-24 10:27:12 +0200

    changed imported names of the variables

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import str from './models/Search'
 
 import {
-    add,
-    multiply,
+    add as a,
+    multiply as m,
     ID
 } from './views/searchView';
 
-console.log(`Using imported functions ${add(ID, 2)} and ${multiply(3,5)}. ${str}`);
+console.log(`Using imported functions ${a(ID, 2)} and ${m(3,5)}. ${str}`);
