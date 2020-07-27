223f70907408f47ba7d8e03cc17c1024e00a5c45
Júlia W <w.julia91@gmail.com>
2020-07-27 21:07:12 +0200

    count update works

--- src/js/index.js
+++ src/js/index.js
@@ -145,6 +145,11 @@ elements.shopping.addEventListener('click', e => {
         state.list.deleteItem(id);
         // Delete from UI
         listView.deleteItem(id);
+
+        // Handle the count update
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
     }
 });
 
