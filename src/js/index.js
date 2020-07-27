46801f18660b049ad3da8d94cc73b75335dcb4e9
Júlia W <w.julia91@gmail.com>
2020-07-27 19:49:33 +0200

    ist modif

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // MVC - CONTROLLER FILE
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import {
@@ -133,5 +134,7 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredient(state.recipe);
     }
-    console.log(state.recipe);
+    
 });
+
+window.l = new List();
