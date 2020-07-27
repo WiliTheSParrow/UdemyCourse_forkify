50c00c58e3c448cc7ded4772a5db1f72cd2db7f4
Júlia W <w.julia91@gmail.com>
2020-07-27 17:48:21 +0200

    inst fractional and measurements formatted

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -36,6 +36,7 @@ export default class Recipe {
     parseIngredients() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsLong, 'kg', 'g'];
         const newIngredients = this.ingredients.map(el => {
 
             // 1) Uniform unites
