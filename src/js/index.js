25c8629ba090e25916ea3af168da45d3798b8314
Júlia W <w.julia91@gmail.com>
2020-07-27 21:01:28 +0200

    item is deleted from shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {
     elements,
     renderLoader,
@@ -16,6 +17,7 @@ import {
 - SHopping list object
 - Liked recipes */
 const state = {};
+window.state = state;
 
 // SEARCH CONTROLLER
 const controlSearch = async () => {
@@ -121,6 +123,36 @@ const controlRecipe = async () => {
 window.addEventListener('load', conrolRecipe); */
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+// LIST CONTROLLER
+const controlList = () => {
+    // Create a new list IF there is none yet.
+    if (!state.list) state.list = new List();
+
+    // Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+        // Delete from UI
+        listView.deleteItem(id);
+    }
+});
+
+
+
+
+
+
 // Handling recipe btn clicks
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -133,8 +165,10 @@ elements.recipe.addEventListener('click', e => {
         // Increase btn is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredient(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
-    
+
 });
 
 window.l = new List();
